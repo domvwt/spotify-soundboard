04e9b70dbf89d32b1056a4eb5069583216e29021
Dominic Thorn <dominic.thorn@gmail.com>
2020-09-02 16:05:00 +0100

    Refactor content to separate module

--- spotify_dash/app.py
+++ spotify_dash/app.py
@@ -1,14 +1,11 @@
 import dash
 import dash_bootstrap_components as dbc
-import dash_core_components as dcc
 import dash_html_components as html
-import dash_table as ddt
-import numpy as np
 from dash.dependencies import Input, Output
-from dash_table.Format import Format
 from flask_caching import Cache
 
 import processes.charts as charts
+import processes.content as cnt
 import processes.views as views
 
 # TODO: Board status - coverage stats: date range, countries, artists, streams; top global genre, artist; last updated
@@ -16,12 +13,10 @@ import processes.views as views
 # TODO: Global settings at top of page - date range
 # TODO: Refactor content to functions and modules for each view
 # TODO: TSNE Viz - 2d / 3d - Colour by continent
+# TODO: Artist Trends
+# TODO: Decouple data pipeline from dashboard - move data to cloud storage
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SOLAR])
-
-SILVER = "rgb(131, 148, 150)"
-SLATE = "rgb(30, 67, 74)"
-DARK_GREY = "rgb(8, 8, 8)"
 
 cache = Cache(
     app.server, config={"CACHE_TYPE": "filesystem", "CACHE_DIR": "cache-directory-app"}
@@ -40,144 +35,6 @@ def cached_world_view():
 @cache.memoize(timeout=TIMEOUT)
 def cached_country_view(country_name):
     return views.country_view(country_name, cached_world_view())
-
-
-def render_world_map():
-    return [
-        html.H1("Global View"),
-        html.Br(),
-        dbc.Row(
-            children=dbc.Col(
-                width=12,
-                children=dbc.Card(
-                    children=[
-                        dbc.CardHeader(html.H3("Stream Atlas", className="card-title")),
-                        dbc.CardBody(
-                            dbc.Jumbotron(
-                                children=dcc.Graph(
-                                    id="world-choropleth",
-                                    figure=charts.world_choropleth(
-                                        views.choropleth_view(cached_world_view())
-                                    ),
-                                    config={"displayModeBar": False},
-                                )
-                            )
-                        ),
-                    ]
-                ),
-            ),
-        ),
-    ]
-
-
-def render_country_profile():
-    country_table_col_dict = [
-        {"name": column, "id": column} for column in ["Position", "Artist", "Genre"]
-    ] + [
-        {
-            "name": "Streams",
-            "id": "Streams",
-            "type": "numeric",
-            "format": Format(group=","),
-        }
-    ]
-
-    return [
-        html.Br(),
-        dbc.Row(dbc.Col(html.H1("Country Profile"))),
-        dbc.Row(
-            children=[
-                dbc.Col(
-                    md=12,
-                    lg=6,
-                    style={"padding-top": 10},
-                    children=dcc.Dropdown(  # DROPDOWN MENU
-                        id="country-input",
-                        options=[
-                            {"label": country, "value": country}
-                            for country in np.sort(
-                                cached_world_view()["Country"].unique()
-                            )
-                        ],
-                        placeholder="Select a country...",
-                    ),
-                ),
-            ],
-        ),
-        html.Br(),
-        dbc.Row(
-            [
-                dbc.Col(
-                    md=12,
-                    lg=6,
-                    children=[
-                        dbc.Card(
-                            style={"margin-bottom": 15},
-                            children=[
-                                dbc.CardHeader(
-                                    html.H3("Stream Breakdown", className="card-title",)
-                                ),
-                                dbc.CardBody(
-                                    [
-                                        dcc.Graph(
-                                            id="country-sunburst",  # COUNTRY SUNBURST CHART
-                                            figure=charts.country_sunburst(
-                                                cached_country_view(
-                                                    country_name="United Kingdom"
-                                                )
-                                            ),
-                                            config={"displayModeBar": False},
-                                        )
-                                    ]
-                                ),
-                            ],
-                        )
-                    ],
-                ),
-                dbc.Col(
-                    md=12,
-                    lg=6,
-                    children=[
-                        dbc.Card(
-                            style={"margin-bottom": 15},
-                            children=[
-                                dbc.CardHeader(
-                                    html.H3("Top Artists", className="card-title")
-                                ),
-                                dbc.CardBody(
-                                    style={"padding-left": 40, "padding-right": 40,},
-                                    children=[
-                                        html.Br(),
-                                        ddt.DataTable(  # COUNTRY TABLE
-                                            id="country-table",
-                                            columns=country_table_col_dict,
-                                            data=cached_country_view(
-                                                country_name="United Kingdom"
-                                            ).to_dict("records"),
-                                            style_header={
-                                                "backgroundColor": SLATE,
-                                                "color": SILVER,
-                                            },
-                                            style_cell={
-                                                "textAlign": "left",
-                                                "backgroundColor": SILVER,
-                                                "color": DARK_GREY,
-                                            },
-                                            style_as_list_view=True,
-                                            sort_action="native",
-                                            page_action="native",
-                                            page_current=0,
-                                            page_size=10,
-                                        ),
-                                    ],
-                                ),
-                            ],
-                        )
-                    ],
-                ),
-            ]
-        ),
-    ]
 
 
 app.layout = html.Div(
@@ -212,9 +69,12 @@ app.layout = html.Div(
             },
             children=[
                 html.Br(),
-                *render_world_map(),
+                *cnt.render_world_map(cached_world_view()),
                 html.Br(),
-                *render_country_profile(),
+                *cnt.render_country_profile(
+                    cached_world_view(),
+                    cached_country_view(country_name="United Kingdom"),
+                ),
                 html.Br(),
             ],
         ),
