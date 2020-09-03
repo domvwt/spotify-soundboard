7f0a50af5fa43a5d24437b7a3de0a6e9d94854dc
Dominic Thorn <dominic.thorn@gmail.com>
2020-09-03 17:09:57 +0100

    Update background decorator

--- spotify_dash/processes/charts.py
+++ spotify_dash/processes/charts.py
@@ -2,38 +2,47 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 
-QUAL_COLS = px.colors.qualitative.Bold
-SILVER = "rgb(131, 148, 150)"
+from processes.content import DEEP_TEAL, SILVER
+
+SEQ_COLS = px.colors.sequential.Agsunset
 
 
-def no_bg(func):
-    def wrapper(*args, **kwargs):
-        fig = func(*args, **kwargs)
-        fig.update_layout(
-            {
-                "plot_bgcolor": "rgba(0, 0, 0, 0)",
-                "paper_bgcolor": "rgba(0, 0, 0, 0)",
-                "font_color": SILVER,
-            }
-        )
-        fig.update_layout(margin=dict(l=10, r=10, t=10, b=10))
-        return fig
+def bg(plot_bg_colour=None, plot_paper_colour=None):
+    plot_bg_colour = "rgba(0, 0, 0, 0)" if plot_bg_colour is None else plot_bg_colour
+    plot_paper_colour = (
+        "rgba(0, 0, 0, 0)" if plot_paper_colour is None else plot_paper_colour
+    )
 
-    return wrapper
+    def bg_helper(func):
+        def wrapper(*args, **kwargs):
+            fig = func(*args, **kwargs)
+            fig.update_layout(
+                {
+                    "plot_bgcolor": plot_bg_colour,
+                    "paper_bgcolor": plot_paper_colour,
+                    "font_color": SILVER,
+                }
+            )
+            fig.update_layout(margin=dict(l=10, r=10, t=10, b=10))
+            return fig
+
+        return wrapper
+
+    return bg_helper
 
 
-@no_bg
+@bg()
 def country_sunburst(chart_data):
     fig = px.sunburst(
         data_frame=chart_data,
         path=["Country", "Genre", "Artist"],
         values="Streams",
-        color_discrete_sequence=QUAL_COLS,
+        color_discrete_sequence=SEQ_COLS,
     )
     return fig
 
 
-@no_bg
+@bg(plot_paper_colour=DEEP_TEAL)
 def world_choropleth(chart_data: pd.DataFrame, scope=None):
     if scope is None:
         scope = "world"
@@ -51,7 +60,7 @@ def world_choropleth(chart_data: pd.DataFrame, scope=None):
             "Top Genre": True,
         },
         locationmode="ISO-3",
-        color_continuous_scale=px.colors.sequential.Agsunset,
+        color_continuous_scale=SEQ_COLS,
         projection="miller",
         scope=scope,
     )
