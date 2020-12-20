443e838c85d357da91d5648c3f378262c2a85a61
Dominic Thorn <dominic.thorn@gmail.com>
2020-12-20 16:42:48 +0000

    Improve download logic

--- spotify_dash/jobs/download_data.py
+++ spotify_dash/jobs/download_data.py
@@ -8,11 +8,19 @@ from pathlib import Path
 
 def download_spotify_asset():
     # Download s3 resources
+    dl_required = False
     asset_path = Path(sts.SPOTIFY_ASSET_PATH)
-    last_update = datetime.fromtimestamp(asset_path.stat().st_ctime)
-    gt_7_days = (datetime.now() - last_update).days > 7
 
-    if not asset_path.is_file() or gt_7_days:
+    if asset_path.is_file():
+        last_update = datetime.fromtimestamp(asset_path.stat().st_ctime)
+        gt_7_days = (datetime.now() - last_update).days > 7
+
+        if gt_7_days:
+            dl_required = True
+    else:
+        dl_required = True
+
+    if dl_required:
         spotify_s3 = s3u.BucketObjectConn(object_name=sts.SPOTIFY_ASSET_PATH.name)
         spotify_s3.download(sts.SPOTIFY_ASSET_PATH)
 
